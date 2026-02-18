e42da8ce7819ce2abffb8112b1267bbe5d76cb81
Itswali <luckywali0986@gmail.com>
2026-02-18 12:07:52 +0500

    completed setup procedure and run the basic api while connecting to db

---
+++ models/itemModel.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const itemModel = new mongoose.Schema({
+
+})
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
